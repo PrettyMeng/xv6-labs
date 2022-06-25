6cdc9435cc43eef78fee081998602825891fe700
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2022-06-26 00:33:59 +0800

    task1 implemented

--- user/uthread.c
+++ user/uthread.c
@@ -2,6 +2,26 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 /* Possible states of a thread: */
 #define FREE        0x0
 #define RUNNING     0x1
@@ -10,10 +30,12 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// extern struct context;
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -35,7 +57,6 @@ void
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
-
   /* Find another runnable thread. */
   next_thread = 0;
   t = current_thread + 1;
@@ -58,6 +79,8 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
+    thread_switch((uint64)(&t->context), (uint64)(&next_thread->context));
+
     /* YOUR CODE HERE
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
@@ -75,7 +98,8 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)&t->stack + 8192;
 }
 
 void 
@@ -96,7 +120,6 @@ thread_a(void)
   a_started = 1;
   while(b_started == 0 || c_started == 0)
     thread_yield();
-  
   for (i = 0; i < 100; i++) {
     printf("thread_a %d\n", i);
     a_n += 1;

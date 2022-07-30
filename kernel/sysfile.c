8df1e4a783938d9e2d79f98be12dc700b99d31db
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2022-07-31 00:40:01 +0800

    passing symlink

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,6 +309,28 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+    if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+      int depth = 0;
+      while (ip->type == T_SYMLINK) {
+        if (readi(ip, 0, (uint64)path, 0, MAXPATH) < 0) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        iunlockput(ip);
+        if((ip = namei(path)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        depth++;
+        if (depth > 10) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+      } 
+    }
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -482,5 +504,35 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+
+uint64
+sys_symlink(void)
+{
+  char path[MAXPATH], target[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  ip = create(path, T_SYMLINK, 0, 0);
+  if (ip == 0) {
+    end_op();
+    return -1; 
+  }
+
+  // write the target name to inode
+  if (writei(ip, 0, (uint64)target, 0, sizeof(target)) < 0) {
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  end_op();
+
   return 0;
 }
